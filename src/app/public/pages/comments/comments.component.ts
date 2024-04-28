6689f05e0d21d002a07d71759163f9c600d30f6a
H4RRY73 <103443967+H4RRY73@users.noreply.github.com>
2024-04-28 18:35:37 -0500

    feat(comments):i update the comment component

--- src/app/public/pages/comments/comments.component.ts
+++ src/app/public/pages/comments/comments.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrl: './comments.component.css'
+  styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent {
   selectedButton: HTMLElement | null = null;
   rating = 0;
+
+  constructor(private snackBar: MatSnackBar, private router: Router) { }
 
   selectButton(event: Event) {
     if (this.selectedButton) {
@@ -16,5 +19,15 @@ export class CommentsComponent {
     }
     this.selectedButton = event.target as HTMLElement;
     this.selectedButton.classList.add('selected');
+  }
+
+  submitComment() {
+    this.snackBar.open('Gracias por tu comentario!!!', '', {
+      duration: 3000,
+    });
+
+    setTimeout(() => {
+      this.router.navigate(['/unirider/maps']);
+    }, 3000);
   }
 }
